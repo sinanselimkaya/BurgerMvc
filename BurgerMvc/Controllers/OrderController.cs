d2fadb09ed3cbd5018fcf0730079d4392daf9605
Sinan Kaya <sinankaya885@gmail.com>
2024-08-17 14:43:37 +0300

    Add project files.

---
+++ BurgerMvc/Controllers/OrderController.cs
@@ -0,0 +1,193 @@
+﻿using AutoMapper;
+using BurgerMvc.BLL.AbstractServices;
+using BurgerMvc.BLL.ConcreteServices;
+using BurgerMvc.BLL.Dtos;
+using BurgerMvc.DAL.Entities;
+using BurgerMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BurgerMvc.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _orderService;
+        private readonly IMapper _mapper;
+        private readonly IOrderCustomerService _orderCustomerService;
+        private readonly IUserService _userService;
+
+        public OrderController(IOrderService orderService, IMapper mapper, IOrderCustomerService orderCustomerService,IUserService userService)
+        {
+            _orderService = orderService;
+            _mapper = mapper;
+            _orderCustomerService = orderCustomerService;
+            _userService = userService;
+        }
+
+        public async Task<IActionResult> Admin()
+        {
+            var menus = await _orderService.GetAllMenu();
+            var allmenus = _mapper.Map<List<MenuItemViewModel>>(menus);
+            var extras = await _orderService.GetAllExtra();
+            var extra = _mapper.Map<List<ExtraIngredientViewModel>>(extras);
+            ViewBag.Extras = extra;
+            return View(allmenus);
+
+        }
+
+        [HttpGet]
+        public IActionResult MenuAdd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> MenuAdd(MenuItemViewModel menuItemViewModel)
+        {
+            if (menuItemViewModel.PhotoUrl != null)
+            {
+                var fileName = Path.GetFileName(menuItemViewModel.PhotoUrl.FileName);
+                var filePath = Path.Combine("wwwroot", "Images", fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await menuItemViewModel.PhotoUrl.CopyToAsync(stream);
+                }
+
+                menuItemViewModel.Photo = fileName;
+            }
+            var menu = _mapper.Map<MenuItemDto>(menuItemViewModel);
+            await _orderService.AddMenu(menu);
+            return RedirectToAction("Admin");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> MenuDelete(int menuItemId)
+        {
+            await _orderService.DeleteMenu(menuItemId);
+            return RedirectToAction("Admin");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> MenuUpdate(int menuItemId)
+        {
+            var menus = await _orderService.GetAllMenu();
+            var menu = menus.FirstOrDefault(x => x.Id == menuItemId);
+            var menuMap = _mapper.Map<MenuItemViewModel>(menu);
+            return View(menuMap);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> MenuUpdate(int menuItemId,IFormFile Photo, MenuItemDto menuItemDto)
+        {
+            if (Photo != null)
+            {
+                var fileName = Path.GetFileName(Photo.FileName);
+                var filePath = Path.Combine("wwwroot", "Images", fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await Photo.CopyToAsync(stream);
+                }
+            }
+            menuItemDto.Photo = Photo.FileName;
+            await _orderService.UpdateMenu(menuItemId, menuItemDto);
+            return RedirectToAction("Admin");
+        }
+
+        [HttpGet]
+        public IActionResult ExtraAdd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ExtraAdd(ExtraIngredientViewModel extraIngredientViewModel)
+        {
+            if (extraIngredientViewModel.PhotoUrl != null)
+            {
+                var fileName = Path.GetFileName(extraIngredientViewModel.PhotoUrl.FileName);
+                var filePath = Path.Combine("wwwroot", "Images", fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await extraIngredientViewModel.PhotoUrl.CopyToAsync(stream);
+                }
+
+                extraIngredientViewModel.Photo = fileName;
+            }
+            var extra = _mapper.Map<ExtraIngredientDto>(extraIngredientViewModel);
+            await _orderService.AddExtra(extra);
+            return RedirectToAction("Admin");
+        }
+        [HttpPost]
+        public async Task<IActionResult> ExtraDelete(int extraId)
+        {
+            await _orderService.DeleteExtra(extraId);
+            return RedirectToAction("Admin");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ExtraUpdate(int extraId)
+        {
+            var extras = await _orderService.GetAllExtra();
+            var extra = extras.FirstOrDefault(x => x.Id == extraId);
+            var extraMap = _mapper.Map<ExtraIngredientViewModel>(extra);
+            return View(extraMap);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ExtraUpdate(int extraId, IFormFile Photo, ExtraIngredientDto extraIngredientDto)
+        {
+            if (Photo != null)
+            {
+                var fileName = Path.GetFileName(Photo.FileName);
+                var filePath = Path.Combine("wwwroot", "Images", fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await Photo.CopyToAsync(stream);
+                }
+            }
+            extraIngredientDto.Photo = Photo.FileName;
+            await _orderService.UpdateExtra(extraId, extraIngredientDto);
+            return RedirectToAction("Admin");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetByOrdersForUser()
+        {
+            var orders = await _orderCustomerService.GetAllOrder();
+            var order = orders.Where(x => x.IsOrderConfirmed == true).OrderByDescending(x => x.ModifiedDate);
+            var ordermap = _mapper.Map<List<OrderViewModel>>(order);
+            return View(ordermap);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> OrderDetail(int orderId, int userId)
+        {
+            var users = await _userService.GetAllUsers();
+            var user = users.FirstOrDefault(x => x.Id == userId);
+            var mapUser = _mapper.Map<UserViewModel>(user);
+
+            var ordersMenus = await _orderCustomerService.GetAllOrdersMenu();
+            var orderMenus = ordersMenus.Where(x => x.OrderId == orderId).ToList();
+            var mapOrderMenus = _mapper.Map<List<OrdersMenuViewModel>>(orderMenus);
+            ViewBag.Ordersmenus = mapOrderMenus;
+
+            var ordersExtras = await _orderCustomerService.GetAllOrdersExtra();
+            var orderExtras = ordersExtras.Where(x => x.OrderId == orderId).ToList();
+            var mapOrderExtras = _mapper.Map<List<OrdersExtraViewModel>>(orderExtras);
+            ViewBag.Ordersextras = mapOrderExtras;
+
+            var menus = await _orderService.GetAllMenu();
+            var mappMenus = _mapper.Map<List<MenuItemViewModel>>(menus);
+            ViewBag.Menus = mappMenus;
+
+            var extras = await _orderService.GetAllExtra();
+            var mapExtras = _mapper.Map<List<ExtraIngredientViewModel>>(extras);
+            ViewBag.Extras = mapExtras;
+
+            return View(mapUser);
+        }
+    }
+}
---
+++ BurgerMvc/Controllers/UserController.cs
@@ -0,0 +1,88 @@
+﻿using AutoMapper;
+using BurgerMvc.BLL.AbstractServices;
+using BurgerMvc.BLL.Dtos;
+using BurgerMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BurgerMvc.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+
+        public UserController(IUserService userService, IMapper mapper)
+        {
+            _userService = userService;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(UserViewModel userViewModel)
+        {
+            if (userViewModel.PhotoUrl != null)
+            {
+                var fileName = Path.GetFileName(userViewModel.PhotoUrl.FileName);
+                var filePath = Path.Combine("wwwroot", "Images", fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await userViewModel.PhotoUrl.CopyToAsync(stream);
+                }
+                userViewModel.Photo = fileName;
+            }
+                var userDto = _mapper.Map<UserDto>(userViewModel);
+                await _userService.Register(userDto);
+                return RedirectToAction("Login");
+            
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            var userDto = await _userService.Login(email, password);
+            if (userDto != null)
+            {
+                var userViewModel = _mapper.Map<UserViewModel>(userDto);
+
+                HttpContext.Session.SetInt32("UserId", userViewModel.Id);
+                HttpContext.Session.SetString("Email", userViewModel.Email);
+                HttpContext.Session.SetString("IsAdmin", userViewModel.IsAdmin.ToString());
+
+                if (userViewModel.IsAdmin == true)
+                {
+                    return RedirectToAction("Admin", "Order", userViewModel);
+
+                }
+                return RedirectToAction("Index", "OrderCustomer", userViewModel);
+            }
+            ViewData["Error"] = "E-posta veya şifre yanlış.";
+            return View();
+        }
+        public async Task<IActionResult> Profille()
+        {
+            var userlogin = HttpContext.Session.GetInt32("UserId");
+
+            var users = await _userService.GetAllUsers();
+            var user = users.FirstOrDefault(x => x.Id == userlogin);
+            var mapUser = _mapper.Map<UserViewModel>(user);
+
+            return View(mapUser);
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear(); //Kullanıcı çıkış yaptığında o kullanıcının giriş bilgileri sıfırlansın.
+            return RedirectToAction("Login");
+        }
+    }
+}
+
